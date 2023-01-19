e6bfea2d6b8a0d1434dccb56f481d37f22204482
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-01-19 20:48:36 +0200

    starting level 2 in day 1

--- Day1/main.js
+++ Day1/main.js
@@ -102,3 +102,21 @@ console.log(removedCompanies1); // ["Facebook", "Google", "Microsoft", "Apple", 
 console.log(itCompanies); //
 
 //Exercise: Level 2
+//1.Create a separate countries.js file and store
+// the countries array into this file, create a separate file web_techs.js
+// and store the webTechs array into this file. Access both file in main.js file
+
+//2.First remove all the punctuations and change the string 
+//to array and count the number of words in the array
+
+//3.In the following shopping cart add, remove, edit items
+//add 'Meat' in the beginning of your shopping cart if it has not been already added
+//add Sugar at the end of you shopping cart if it has not been already added
+//remove 'Honey' if you are allergic to honey
+//modify Tea to 'Green Tea'
+
+//4.In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+//
+//5.In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+//
+//6.Concatenate the following two variables and store it in a fullStack variable.
