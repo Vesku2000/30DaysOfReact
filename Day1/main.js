9e91a66b62dd6a38fbed78dd6cff6f30fb51f9b5
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-01-21 14:41:03 +0200

    level 2 done

--- Day1/main.js
+++ Day1/main.js
@@ -165,3 +165,7 @@ if (webTechs.includes("Sass")) {
     console.log(webTechs);
 }
 //6.Concatenate the following two variables and store it in a fullStack variable.
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node', 'Express', 'MongoDB']
+const fullStack = [...frontEnd, ...backEnd];
+console.log(fullStack)
