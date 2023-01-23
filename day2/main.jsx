0f0e42e7acd33711ced594b2e721e77e0a244b2f
Valtteri <102945073+Vesku2000@users.noreply.github.com>
2023-01-23 15:29:56 +0000

    tasks

---
+++ day2/main.jsx
@@ -0,0 +1,13 @@
+//What is an HTML element?
+//An HTML element is defined by a start tag, some content, and an end tag.
+//How to write a self closing HTML element?
+//<div />
+//What is an HTML attribute? Write some of them
+//What is JSX?
+//What is babel?
+//What is a transpiler?
+//Exercises: JSX Elements
+//What is a JSX element?
+//Write your name in a JSX element and store it in a name //variable
+//Write a JSX element which displays your full name, country, //title, gender, email, phone number. Use h1 for the name and //p for the rest of the information and store it in a user //variable
+//Write a footer JSX element
